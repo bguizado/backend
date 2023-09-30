ab28369d68e8ecf1d1b6088777fea01e7676cce9
bguizado <brando.guizado.87@gmail.com>
2023-09-30 08:24:35 -0500

    Actualizando para renderizar5

--- pointdata/pointdata/settings.py
+++ pointdata/pointdata/settings.py
@@ -29,7 +29,11 @@ SECRET_KEY = environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'pointdata.onrender.com',
+    '127.0.0.1',
+    'localhost'
+]
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
